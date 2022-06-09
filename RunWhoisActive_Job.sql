c5a0db654f1f109fc0a16c3e4ce48bef51262845
NagaKatrapati <107057196+NagaKatrapati@users.noreply.github.com>
2022-06-09 19:56:03 +0530

    Add files via upload

---
+++ RunWhoisActive_Job.sql
@@ -0,0 +1,115 @@
+
+Use PRODDBA
+GO
+
+CREATE TABLE [dbo].[WhoIsActive](
+	[dd hh:mm:ss.mss] [varchar](8000) NULL,
+	[session_id] [smallint] NOT NULL,
+	[sql_text] [xml] NULL,
+	[sql_command] [xml] NULL,
+	[login_name] [nvarchar](128) NOT NULL,
+	[wait_info] [nvarchar](4000) NULL,
+	[tran_log_writes] [nvarchar](4000) NULL,
+	[CPU] [varchar](30) NULL,
+	[tempdb_allocations] [varchar](30) NULL,
+	[tempdb_current] [varchar](30) NULL,
+	[blocking_session_id] [smallint] NULL,
+	[blocked_session_count] [varchar](30) NULL,
+	[reads] [varchar](30) NULL,
+	[writes] [varchar](30) NULL,
+	[physical_reads] [varchar](30) NULL,
+	[locks] [xml] NULL,
+	[used_memory] [varchar](30) NULL,
+	[status] [varchar](30) NOT NULL,
+	[implicit_tran] [varchar](30) NULL,
+	[tran_start_time] [varchar](30) NULL,
+	[open_tran_count] [varchar](30) NULL,
+	[percent_complete] [varchar](30) NULL,
+	[host_name] [nvarchar](128) NULL,
+	[database_name] [nvarchar](128) NULL,
+	[program_name] [nvarchar](128) NULL,
+	[additional_info] [xml] NULL,
+	[start_time] [datetime] NOT NULL,
+	[login_time] [datetime] NULL,
+	[request_id] [int] NULL,
+	[collection_time] [datetime] NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+USE [msdb]
+GO
+
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 6/9/2022 7:53:05 PM ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'Run WhoIsActive', 
+		@enabled=0, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=0, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'No description available.', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'sa', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Step1]    Script Date: 6/9/2022 7:53:05 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Step1', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'DECLARE  @destination_table VARCHAR(500) = ''WhoIsActive'',
+	@destination_database sysname = ''PRODDBA'';
+	SET @destination_table = @destination_database + ''.dbo.'' + @destination_table;
+ 
+	--collect activity into logging table
+	EXEC dbo.sp_WhoIsActive @get_transaction_info = 1,
+			@get_outer_command = 1,
+			@get_locks = 1,
+			@find_block_leaders=1,
+			@get_additional_info=1,
+			@destination_table = @destination_table;', 
+		@database_name=N'master', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Every10Sec-WIA', 
+		@enabled=1, 
+		@freq_type=4, 
+		@freq_interval=1, 
+		@freq_subday_type=2, 
+		@freq_subday_interval=10, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=0, 
+		@active_start_date=20210201, 
+		@active_end_date=99991231, 
+		@active_start_time=0, 
+		@active_end_time=235959
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+GO
+
+
